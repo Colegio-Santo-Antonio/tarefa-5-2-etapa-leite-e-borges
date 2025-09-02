4533ed0699d8a2486ba0ba2a3cdd14dbfce78860
BeatrizBorges05 <csa25-2792@colegiosantoantonio.com.br>
2025-09-02 16:39:37 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,15 +1,15 @@
 numero=input()
-impares=[]
+impares=[ ]
 for i in numero[-1::-2]:
     impares.append (int(i))
-pares=[]
+pares = []
 for i in numero[-2::-2]:
-    if 2*int(i)<10:
-        pares.append(2*int(i))
-    else:
-        pares.append(2*int(i)-10+1)
-    soma = sum(impares)+sum(pares)
-    if int(soma/10) == soma/10:
-       print("cartão válido")
-    else:
-       print("cartão inválido")
+  if 2*int(i)<10:
+    pares.append(2*int(i))
+  else:
+    pares.append(2*int(i)-10+1)
+ soma = sum(impares)+sum(pares)
+ if int(soma/10) == soma/10:
+   print("cartão válido")
+ else:
+   print("cartão inválido")
